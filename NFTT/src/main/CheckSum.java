214b66ea384b4f148dabd18348e4e397c2d966ea
Kevin Liu <krliu@cpp.edu>
2016-11-30 00:18:32 -0800

    nmee

--- NFTT/src/main/CheckSum.java
+++ NFTT/src/main/CheckSum.java
@@ -55,7 +55,7 @@ public class CheckSum {
 		for (int i = 0; i  < array.length; i++){
 			System.out.println(i);
 			// System.out.println(order.charAt(count % 8));
-			switch (orderArray[count] % 8) {
+			switch (orderArray[count]) {
 			case 0:
 				//System.out.println("0");
 				piece1 = new BigInteger("" + (long) Math.abs((Math.cos(array[i]) * 5000 * factor)));
@@ -126,7 +126,7 @@ public class CheckSum {
 
 			count = (count + 1) % 8;
 
-			temp = temp.mod(new BigInteger("1298074214633706835075030044377087"));
+			//temp = temp.mod(new BigInteger("1298074214633706835075030044377087"));
 		}
 
 		temp = temp.mod(new BigInteger("1298074214633706835075030044377087"));
